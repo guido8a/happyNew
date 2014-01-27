0e1ab63330775ffa05fd6e68272bbba82abc9e92
luz <luzma_87@yahoo.com>
2014-01-27 13:48:20 -0500

    inicio

--- grails-app/controllers/happy/seguridad/InicioController.groovy
+++ grails-app/controllers/happy/seguridad/InicioController.groovy
@@ -1,13 +1,15 @@
 package happy.seguridad
 
-class InicioController {
+class InicioController extends happy.seguridad.Shield {
 
     def index() {
-        def usu = Persona.get(session.usuario.id)
-        def now = new Date().clearTime()
-        if ((usu.password == usu.cedula.encodeAsMD5()) || usu.fechaCambioPass <= now) {
-            redirect(controller: "login", action: "cambiarPass")
-            return
+        def usu = Persona.get(session.usuario?.id)
+        if (usu) {
+            def now = new Date().clearTime()
+            if ((usu.password == usu.cedula.encodeAsMD5()) || usu.fechaCambioPass <= now) {
+                redirect(controller: "login", action: "cambiarPass")
+                return
+            }
         }
     }
 
