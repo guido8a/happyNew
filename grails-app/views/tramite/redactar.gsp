8522611700f2dbd52ea9504b68ca5080221e7c4b
luz <luzma_87@yahoo.com>
2014-02-06 15:54:05 -0500

    ckeditor

--- grails-app/views/tramite/redactar.gsp
+++ grails-app/views/tramite/redactar.gsp
@@ -25,7 +25,22 @@
 
                 if (typeof CKEDITOR != "undefined") {
                     $('textarea.editor').ckeditor({
-                        height : 600
+                        height  : 600,
+                        toolbar : [
+                            [ 'Save', 'NewPage', 'Preview', 'Print' ],
+                            [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ],
+                            [ 'Find', 'Replace', '-', 'SelectAll' ],
+                            [ 'Image', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ],
+                            '/',
+                            [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ],
+                            [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ],
+                            '/',
+                            [ 'Styles', 'Format', 'Font', 'FontSize' ],
+                            [ 'TextColor', 'BGColor' ] ,
+                            [ 'Maximize' ],
+                            [ '-' ] ,
+                            [ 'About' ]
+                        ]
                     });
                 }
             });
--- web-app/js/plugins/ckeditor/config.js
+++ web-app/js/plugins/ckeditor/config.js
@@ -8,11 +8,52 @@ CKEDITOR.editorConfig = function (config) {
     // %REMOVE_START%
     // The configuration options below are needed when running CKEditor from source files.
 //    config.plugins = 'dialogui,dialog,about,a11yhelp,dialogadvtab,basicstyles,bidi,blockquote,clipboard,button,panelbutton,panel,floatpanel,colorbutton,colordialog,templates,menu,contextmenu,div,resize,toolbar,elementspath,enterkey,entities,popup,filebrowser,find,fakeobjects,flash,floatingspace,listblock,richcombo,font,forms,format,horizontalrule,htmlwriter,iframe,wysiwygarea,image,indent,indentblock,indentlist,smiley,justify,menubutton,language,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastetext,pastefromword,preview,print,removeformat,save,selectall,showblocks,showborders,sourcearea,specialchar,scayt,stylescombo,tab,table,tabletools,undo,wsc,autosave,eqneditor,wordcount';
-//    config.skin = 'moonocolor';
-//    config.language = "es";
+    config.skin = 'moonocolor';
+    config.language = "es";
     // %REMOVE_END%
 
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
-    // config.uiColor = '#AADC6E';
+    config.uiColor = '#4D76A3';
+
+    /*
+     // Toolbar configuration generated automatically by the editor based on config.toolbarGroups.
+     config.toolbar = [
+     { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] },
+     { name: 'clipboard', groups: [ 'clipboard', 'undo' ], items: [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] },
+     { name: 'editing', groups: [ 'find', 'selection', 'spellchecker' ], items: [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ] },
+     { name: 'forms', items: [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+     '/',
+     { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ] },
+     { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ], items: [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ] },
+     { name: 'links', items: [ 'Link', 'Unlink', 'Anchor' ] },
+     { name: 'insert', items: [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ] },
+     '/',
+     { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+     { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
+     { name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
+     { name: 'others', items: [ '-' ] },
+     { name: 'about', items: [ 'About' ] }
+     ];
+     */
+
+    /*
+     config.toolbar = [
+     [ 'Source', '-', 'Save', 'NewPage', 'Preview', 'Print', '-', 'Templates' ] ,
+     [ 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' ] ,
+     [ 'Find', 'Replace', '-', 'SelectAll', '-', 'Scayt' ],
+     [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ],
+     '/',
+     [ 'Bold', 'Italic', 'Underline', 'Strike', 'Subscript', 'Superscript', '-', 'RemoveFormat' ],
+     [ 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote', 'CreateDiv', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock', '-', 'BidiLtr', 'BidiRtl', 'Language' ],
+     [ 'Link', 'Unlink', 'Anchor' ] ,
+     [ 'Image', 'Flash', 'Table', 'HorizontalRule', 'Smiley', 'SpecialChar', 'PageBreak', 'Iframe' ],
+     '/',
+     [ 'Styles', 'Format', 'Font', 'FontSize' ],
+     [ 'TextColor', 'BGColor' ] ,
+     [ 'Maximize', 'ShowBlocks' ],
+     [ '-' ] ,
+     [ 'About' ]
+     ]
+     */
 };
