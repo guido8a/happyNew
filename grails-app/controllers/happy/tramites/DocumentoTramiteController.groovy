16ed62c6949979843f20529cdd9f3a37a062dad3
valentinsvt <valentinsvt@hotmail.com>
2014-04-28 16:34:36 -0500

    despues del cambio de path

--- grails-app/controllers/happy/tramites/DocumentoTramiteController.groovy
+++ grails-app/controllers/happy/tramites/DocumentoTramiteController.groovy
@@ -14,113 +14,6 @@ class DocumentoTramiteController extends happy.seguridad.Shield {
 
     static allowedMethods = [save: "POST", delete: "POST", save_ajax: "POST", delete_ajax: "POST"]
 
-//    def index() {
-//        redirect(action: "list", params: params)
-//    } //index
-//
-//    def getLista(params, all) {
-//        params = params.clone()
-//        if (all) {
-//            params.remove("offset")
-//            params.remove("max")
-//        }
-//        def lista
-//        if (params.search) {
-//            def c = DocumentoTramite.createCriteria()
-//            lista = c.list(params) {
-//                or {
-//                    ilike("resumen", "%" + params.search + "%")
-//                    ilike("clave", "%" + params.search + "%")
-//                    ilike("descripcion", "%" + params.search + "%")
-//                }
-//            }
-//        } else {
-//            lista = DocumentoTramite.list(params)
-//        }
-//        return lista
-//    }
-//
-//    def list() {
-//        params.max = Math.min(params.max ? params.max.toInteger() : 10, 100)
-//        def documentoTramiteInstanceList = getLista(params, false)
-//        def documentoTramiteInstanceCount = getLista(params, true).size()
-//        if (documentoTramiteInstanceList.size() == 0 && params.offset && params.max) {
-//            params.offset = params.offset - params.max
-//        }
-//        documentoTramiteInstanceList = getLista(params, false)
-//        return [documentoTramiteInstanceList: documentoTramiteInstanceList, documentoTramiteInstanceCount: documentoTramiteInstanceCount, params: params]
-//    } //list
-//
-//    def show_ajax() {
-//        if (params.id) {
-//            def documentoTramiteInstance = DocumentoTramite.get(params.id)
-//            if (!documentoTramiteInstance) {
-//                notFound_ajax()
-//                return
-//            }
-//            return [documentoTramiteInstance: documentoTramiteInstance]
-//        } else {
-//            notFound_ajax()
-//        }
-//    } //show para cargar con ajax en un dialog
-//
-//    def form_ajax() {
-//        def documentoTramiteInstance = new DocumentoTramite(params)
-//        if (params.id) {
-//            documentoTramiteInstance = DocumentoTramite.get(params.id)
-//            if (!documentoTramiteInstance) {
-//                notFound_ajax()
-//                return
-//            }
-//        }
-//        return [documentoTramiteInstance: documentoTramiteInstance]
-//    } //form para cargar con ajax en un dialog
-//
-//    def save_ajax() {
-//        params.each { k, v ->
-//            if (v != "date.struct" && v instanceof java.lang.String) {
-//                params[k] = v.toUpperCase()
-//            }
-//        }
-//        def documentoTramiteInstance = new DocumentoTramite()
-//        if (params.id) {
-//            documentoTramiteInstance = DocumentoTramite.get(params.id)
-//            if (!documentoTramiteInstance) {
-//                notFound_ajax()
-//                return
-//            }
-//        } //update
-//        documentoTramiteInstance.properties = params
-//        if (!documentoTramiteInstance.save(flush: true)) {
-//            def msg = "NO_No se pudo ${params.id ? 'actualizar' : 'crear'} DocumentoTramite."
-//            msg += renderErrors(bean: documentoTramiteInstance)
-//            render msg
-//            return
-//        }
-//        render "OK_${params.id ? 'Actualización' : 'Creación'} de DocumentoTramite exitosa."
-//    } //save para grabar desde ajax
-//
-//    def delete_ajax() {
-//        if (params.id) {
-//            def documentoTramiteInstance = DocumentoTramite.get(params.id)
-//            if (documentoTramiteInstance) {
-//                try {
-//                    documentoTramiteInstance.delete(flush: true)
-//                    render "OK_Eliminación de DocumentoTramite exitosa."
-//                } catch (e) {
-//                    render "NO_No se pudo eliminar DocumentoTramite."
-//                }
-//            } else {
-//                notFound_ajax()
-//            }
-//        } else {
-//            notFound_ajax()
-//        }
-//    } //delete para eliminar via ajax
-//
-//    protected void notFound_ajax() {
-//        render "NO_No se encontró DocumentoTramite."
-//    } //notFound para ajax
 
     def anexo() {
         def tramite = Tramite.get(params.id)
@@ -167,10 +60,13 @@ class DocumentoTramiteController extends happy.seguridad.Shield {
     def borrarDoc(){
         if(request.getMethod()=="POST"){
             def doc = DocumentoTramite.get(params.id)
+            def departamento = doc.tramite.deDepartamento
+            if(!departamento)
+                departamento=doc.tramite.de.departamento
             if(doc.tramite.estadoTramite.codigo=="E001" || doc.tramite.estadoTramite.codigo=="E002"  ){
                 def band =true
                 try{
-                    def path = servletContext.getRealPath("/") + "anexos/" + doc.tramite.id+ "/"+doc.path
+                    def path = servletContext.getRealPath("/") + "anexos/${departamento.codigo}/" + doc.tramite.codigo+ "/"+doc.path
                     def file = new File(path)
                     file.delete()
                 }catch (e){
@@ -202,9 +98,12 @@ class DocumentoTramiteController extends happy.seguridad.Shield {
 
     def descargarDoc(){
         def doc = DocumentoTramite.get(params.id)
+        def departamento = doc.tramite.deDepartamento
+        if(!departamento)
+            departamento=doc.tramite.de.departamento
         if(session.key == (doc.path.size()+doc.resumen?.encodeAsMD5().substring(0,10))){
             session.key = null
-            def path = servletContext.getRealPath("/") + "anexos/" + doc.tramite.id+ "/"+doc.path
+            def path = servletContext.getRealPath("/") + "anexos/${departamento.codigo}/" + doc.tramite.codigo+ "/"+doc.path
             println "path "+doc.path+" "+doc.path.split("\\.")
             def tipo = doc.path.split("\\.")
             tipo = tipo[1]
@@ -248,7 +147,7 @@ class DocumentoTramiteController extends happy.seguridad.Shield {
     def uploadSvt(){
         println "updaload svt "+params
         def tramite = Tramite.get(params.id)
-        def path = servletContext.getRealPath("/") + "anexos/" + tramite.id+ "/"    //web-app/archivos
+        def path = servletContext.getRealPath("/") + "anexos/${session.departamento.codigo}/" + tramite.codigo+ "/"    //web-app/archivos
         new File(path).mkdirs()
         def f = request.getFile('file')  //archivo = name del input type file
         def imageContent = ['image/png': "png", 'image/jpeg': "jpeg", 'image/jpg': "jpg"]
@@ -370,7 +269,7 @@ class DocumentoTramiteController extends happy.seguridad.Shield {
                             files: [
                                     [
                                             name: nombre,
-                                            url : resource(dir: 'anexos/' + tramite.id, file: nombre),
+                                            url : resource(dir: "anexos/${session.departamento.codigo}/" + tramite.codigo, file: nombre),
                                             size: f.getSize(),
                                             url : pathFile
                                     ]
