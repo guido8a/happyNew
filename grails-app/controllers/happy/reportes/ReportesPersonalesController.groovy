29ca19b6ec84a1d59232992fd140580b9d645789
luzma87 <luzma_87@yahoo.com>
2014-07-10 13:50:16 -0500

    cambio links reporte retrasados

--- grails-app/controllers/happy/reportes/ReportesPersonalesController.groovy
+++ grails-app/controllers/happy/reportes/ReportesPersonalesController.groovy
@@ -9,22 +9,4 @@ class ReportesPersonalesController extends Shield {
         def usu = Persona.get(session.usuario.id)
         return [persona: usu]
     }
-
-    def jefe() {
-        def usu = Persona.get(session.usuario.id)
-        if (usu.puedeJefe) {
-            return [persona: usu]
-        } else {
-            redirect(action: "personal")
-        }
-    }
-
-    def director() {
-        def usu = Persona.get(session.usuario.id)
-        if (usu.puedeDirector) {
-            return [persona: usu]
-        } else {
-            redirect(action: "personal")
-        }
-    }
 }
