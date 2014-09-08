b04254d725480ccea00955cccc35eb832c651998
valentinsvt <valentinsvt@hotmail.com>
2014-09-08 11:10:45 -0500

    nuevo reporte

--- grails-app/controllers/happy/seguridad/LoginController.groovy
+++ grails-app/controllers/happy/seguridad/LoginController.groovy
@@ -219,11 +219,11 @@ class LoginController {
                         if (count > 0)
                             redirect(controller: 'alertas', action: 'list')
                         else {//
-                            if(session.usuario.getPuedeJefe()){
-                                redirect(controller: "retrasadosWeb", action: "reporteRetrasadosConsolidado", params: [dpto: Persona.get(session.usuario.id).departamento.id],inicio:"1")
+                            if(session.usuario.getPuedeDirector()){
+                                redirect(controller: "retrasadosWeb", action: "reporteRetrasadosConsolidadoDir", params: [dpto: Persona.get(session.usuario.id).departamento.id,inicio:"1",dir:"1"])
                             }else{
-                                if(session.usuario.getPuedeDirector()){
-                                    redirect(controller: "retrasadosWeb", action: "reporteRetrasadosConsolidado", params: [dpto: Persona.get(session.usuario.id).departamento.id,inicio:"1"])
+                                if(session.usuario.getPuedeJefe()){
+                                    redirect(controller: "retrasadosWeb", action: "reporteRetrasadosConsolidado", params: [dpto: Persona.get(session.usuario.id).departamento.id],inicio:"1")
                                 }else{
                                     redirect(controller: "inicio", action: "index")
                                 }
