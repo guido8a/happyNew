765a15c9b790c7cbc6a596aea2ad291f0db82c57
fabricio4077 <nightcat4077@hotmail.com>
2014-10-21 11:51:52 -0500

    arreglo de error en reporte excel de generados

--- grails-app/controllers/happy/reportes/RetrasadosExcelController.groovy
+++ grails-app/controllers/happy/reportes/RetrasadosExcelController.groovy
@@ -302,7 +302,7 @@ class RetrasadosExcelController {
                 if (lvl["tramites"].size() > 0) {
                     def triangulo=""
                     if(lvl["triangulos"] && lvl["triangulos"].size()>0){
-                        triangulo = lvl["triangulos"].get(0)+" (Oficina)"
+                        triangulo = lvl["triangulos"].get(0).toString() +" (Oficina)"
                     }else{
                         triangulo="Oficina"
                     }
@@ -310,8 +310,8 @@ class RetrasadosExcelController {
                     lvl["tramites"].each { t ->
                         num++
                         row = sheet.createRow((short) num);
-                        row.createCell((int) 1).setCellValue(${(t.fechaRecepcion)?'Retrasado':'Sin recepción'})
-                        row.createCell((int) 0).setCellValue(triangulo)
+                        row.createCell((int) 0).setCellValue("${(t.fechaRecepcion)?'Retrasado':'Sin recepción'}")
+                        row.createCell((int) 1).setCellValue(triangulo)
                         row.createCell((int) 2).setCellValue("${t.tramite.codigo} ${t.rolPersonaTramite.codigo == 'R002' ? '[CC]' : ''}")
                         row.createCell((int) 3).setCellValue("${t.tramite.fechaCreacion.format('dd-MM-yyyy HH:mm')}")
                         if (t.tramite.deDepartamento) {
