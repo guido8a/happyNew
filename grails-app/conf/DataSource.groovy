e20019893bce8ee421d0867701386cade377c6d6
guido8a <guido8a@gmail.com>
2015-07-23 08:10:19 -0500

    datasource

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -4,8 +4,8 @@ dataSource {
     dialect = org.hibernate.dialect.PostgreSQLDialect
 }
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = true
+    cache.use_second_level_cache = false
+    cache.use_query_cache = false
     cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
@@ -13,8 +13,8 @@ environments {
     development {
         dataSource {
             dbCreate = "update"
-//            url = "jdbc:postgresql://10.0.0.2:5432/sadweb_/prdc1"
-            url = "jdbc:postgresql://10.0.0.2:5432/sadweb_prdc"
+//            url = "jdbc:postgresql://10.0.0.2:5432/sadweb_prdc"
+            url = "jdbc:postgresql://127.0.0.1:5432/sadweb_prdc"
 //            url = "jdbc:postgresql://10.0.0.2:5432/happy10"
             username = "postgres"
             password = "postgres"
