11081bdb74673a8c3d4e4b32596739b05a733a36
guido8a <guido8a@gmail.com>
2015-08-17 12:28:14 -0500

    pool de 20 variable

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -7,14 +7,13 @@ hibernate {
     cache.use_second_level_cache = false
     cache.use_query_cache = false
     cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
-    connection.pool_size=3
 }
 // environment specific settings
 environments {
     development {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:postgresql://10.0.0.2:5432/sadweb_brre3"
+            url = "jdbc:postgresql://10.0.0.2:5432/sadweb_brre1"
 //            url = "jdbc:postgresql://10.0.0.2:5432/sadweb_prdc"
 //            url = "jdbc:postgresql://127.0.0.1:5432/sadweb_prdc"
 //            url = "jdbc:postgresql://10.0.0.2:5432/happy10"
