e6f4e154a0784396cef2bc52d0047c59dd24f584
guido8a <guido8a@gmail.com>
2015-08-25 12:41:41 -0500

    parametros de la BD

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -22,7 +22,7 @@ environments {
 
             properties {
                 jmxEnabled = true
-                initialSize = 20
+                initialSize = 10
                 maxActive = 100
                 minIdle = 10
                 maxIdle = 100
@@ -59,7 +59,7 @@ environments {
 
         properties {
             jmxEnabled = true
-            initialSize = 20
+            initialSize = 10
             maxActive = 100
             minIdle = 10
             maxIdle = 100
@@ -100,13 +100,13 @@ environments {
                 maxActive = 100
                 minIdle = 10
                 maxIdle = 100
-                maxWait = 30000         // 10 segundos
+                maxWait = 60000         // 10 segundos
 //                maxAge = 30 * 60000     // 0 por defecto: No se chequea las conexiones
-                timeBetweenEvictionRunsMillis = 30000   //1800000
-                minEvictableIdleTimeMillis = 60000
+                timeBetweenEvictionRunsMillis = 60000   //1800000
+                minEvictableIdleTimeMillis = 120000
                 validationQuery = "SELECT 1"
                 validationQueryTimeout = 3
-                validationInterval = 30000
+                validationInterval = 60000
                 testOnBorrow = true
                 testWhileIdle = true
                 testOnReturn = false
@@ -118,50 +118,4 @@ environments {
 
 }
 
-/*
-dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
-}
-hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
-//    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
-}
 
-// environment specific settings
-environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=false
-               validationQuery="SELECT 1"
-               jdbcInterceptors="ConnectionState"
-            }
-        }
-    }
-}
-*/
