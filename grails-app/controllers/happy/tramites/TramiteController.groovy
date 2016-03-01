60af02b70eb58ed4c546ca7e09155470f06b3814
guido8a <guido8a@gmail.com>
2016-03-01 14:15:06 -0500

    para en crear trámites

--- grails-app/controllers/happy/tramites/TramiteController.groovy
+++ grails-app/controllers/happy/tramites/TramiteController.groovy
@@ -249,7 +249,9 @@ class TramiteController extends happy.seguridad.Shield {
     }
 
     def getParaNuevo_ajax() {
-        def sql = "SELECT * FROM trmt_para($session.usuario.id)"
+        println "getParaNuevo_ajax $params, perfil${session.perfil.id}"
+        def sql = "SELECT * FROM trmt_para(${session.usuario.id}, ${session.perfil.id})"
+//        def sql = "SELECT * FROM trmt_para(${session.usuario.id})"
         def cn = dbConnectionService.getConnection()
         def rows = cn.rows(sql.toString())
 
@@ -642,7 +644,7 @@ class TramiteController extends happy.seguridad.Shield {
  */
 
 
-        def sql = "SELECT id, dscr as label, externo FROM trmt_para($session.usuario.id)"
+        def sql = "SELECT id, dscr as label, externo FROM trmt_para(${session.usuario.id}, ${session.perfil.id})"
         def cn = dbConnectionService.getConnection()
         todos = cn.rows(sql.toString())
 //
