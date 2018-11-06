2312992ee2a01288184269e68f343f49c5ce8c3e
guido8a <guido8a@gmail.com>
2018-11-06 13:14:18 -0500

    Usos ok

--- grails-app/controllers/happy/seguridad/Shield.groovy
+++ grails-app/controllers/happy/seguridad/Shield.groovy
@@ -1,10 +1,13 @@
 package happy.seguridad
 
-
+import groovy.sql.Sql
+import groovy.time.TimeCategory
 import happy.tramites.Departamento
 
 
 class Shield {
+    def dataSource
+
     def beforeInterceptor = [action: this.&auth, except: 'login']
 
     /**
@@ -13,20 +16,14 @@ class Shield {
      */
     def auth() {
 //        println "an " + actionName + " cn " + controllerName + "  "
-//        println "shield sesión: " + session.usuario
         session.an = actionName
         session.cn = controllerName
         session.pr = params
 //        return true
 
-//        def para = alertaNoRecibidos().tramitesPasados
-
-        /** **************************************************************************/
-
+        /* si está en redactar guarda el texto */
         if(session.an == 'saveTramite' && session.cn == 'tramite'){
-//            println("entro")
             return true
-
         }else{
             if (!session.usuario || !session.perfil) {
                 if(controllerName != "inicio" && actionName != "index") {
@@ -35,6 +32,7 @@ class Shield {
                 render "<script type='text/javascript'> window.location.href = '${createLink(controller:'login', action:'login')}'; </script>"
                 session.finalize()
                 return false
+
             } else {
                 def now = new Date()
                 def band = true
@@ -54,13 +52,48 @@ class Shield {
 //                redirect(controller: 'login', action: 'finDeSesion')
 //                return false
 //            }
+
+
+//                println "---> $controllerName / $actionName"
+                /* inicio de recgistro de actividad del usaurio */
+                Sql sql = new Sql(dataSource)
+                def sale = new Date().format("yyyy-MM-dd HH:mm:ss")
+                def fcha = new Date().format("yyyy-MM-dd")
+                def tx = "select accn__id, tpac__id from accn, ctrl where accnnmbr ilike '${actionName}' and " +
+                        "ctrl.ctrl__id = accn.ctrl__id and ctrlnmbr ilike '${controllerName}'"
+//                println "---> ${entero(session.id)}"
+                def accn__id, tipo
+                sql.eachRow(tx.toString()) { d ->
+                    accn__id = d.accn__id
+                    tipo     = d.tpac__id
+                }
+
+                tx = "select accn__id, usst__id from usst where usstsesn = ${entero(session.id)} " +
+                        "order by usst__id desc limit 1"
+                def accndsde, id
+                sql.eachRow(tx.toString()) { d ->
+                    accndsde = d.accn__id
+                    id       = d.usst__id
+                }
+
+//                println "accn: actual ${accn__id}, anterior: ${accndsde}, tipo: $tipo"
+                if((accn__id != accndsde) && tipo == 1) {
+                    tx = "update usst set usstfcsa = '${sale}' where usst__id = ${id}"
+                    sql.execute(tx.toString())
+
+                    tx = "insert into usst(prfl__id, accn__id, accndsde, prsn__id, usstfcen, usstsesn) values (" +
+                            "${session.perfil.id}, ${accn__id}, ${accndsde}, ${session.usuario.id}, '${fcha}', " +
+                            "${entero(session.id)})"
+                    sql.execute(tx.toString())
+                }
+//                def fin = new Date()
+//                println "${TimeCategory.minus(fin, now)}"
+
+                /* fin de recgistro de actividad del usaurio */
+
                 def usu = Persona.get(session.usuario.id)
 //                println("usuario activo: " + usu.estaActivo)
                 if (usu.estaActivo) {
-
-//                println "AQUI??????"
-//                println "controlador: $controllerName acción: $actionName"
-
                     session.departamento = Departamento.get(session.departamento.id).refresh()
                     def perms = session.usuario.permisos
                     session.usuario = Persona.get(session.usuario.id).refresh()
@@ -178,8 +211,18 @@ class Shield {
             println "Shield execption e: " + e
             return false
         }
-
         return false
+    }
+
+
+    def entero(s) {
+        int val = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int num = (int) c;
+            val += num;
+        }
+        val
     }
 
 
